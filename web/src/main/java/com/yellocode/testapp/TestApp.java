8b966b1907c4278d9bb86106075836e064afd07b
adomorenok <overgrovnplesen@mail.ru>
2017-04-08 17:58:52 +0300

    privet

--- web/src/main/java/com/yellocode/testapp/TestApp.java
+++ web/src/main/java/com/yellocode/testapp/TestApp.java
@@ -6,7 +6,7 @@ package com.yellocode.testapp;
 public class TestApp {
 
     public static void main(String[] args) {
-        System.out.println("Result: " + new CheckString().check(""));
+        System.out.println("Privet Dima!!!");
     }
 
 }
