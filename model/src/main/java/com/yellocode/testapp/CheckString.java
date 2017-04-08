f3266aa770e40ada7e5f1c8592c8192e935a0e94
adomorenok <overgrovnplesen@mail.ru>
2017-04-08 17:55:29 +0300

    implement first test

---
+++ model/src/main/java/com/yellocode/testapp/CheckString.java
@@ -0,0 +1,14 @@
+package com.yellocode.testapp;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by ikota on 8.4.17.
+ */
+public class CheckString {
+
+    public boolean check(String str) {
+        return StringUtils.isBlank(str);
+    }
+
+}
---
+++ web/src/main/java/com/yellocode/testapp/TestApp.java
@@ -0,0 +1,12 @@
+package com.yellocode.testapp;
+
+/**
+ * Created by ikota on 8.4.17.
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        System.out.println("Result: " + new CheckString().check(""));
+    }
+
+}
